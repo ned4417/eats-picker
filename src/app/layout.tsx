cf9f3f907fbd539b5b9be4cad13422c49725ffd3
Eddie Tuell <etuell@chas.org>
2025-03-07 14:59:13 -0800

    fix: Vercel Speed Insights
    installed Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -84,6 +85,8 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
+        
       </body>
     </html>
   );
